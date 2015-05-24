64ea73f1fe5f4de4491d315787342793c7c735cb
Thomas Sundberg <tsu@kth.se>
2015-05-24 12:11:11 +0200

    Added http://selenium.thinkcode.se as a possible base url

--- src/test/java/se/thinkcode/selenium/TestHelper.java
+++ src/test/java/se/thinkcode/selenium/TestHelper.java
@@ -2,6 +2,7 @@ package se.thinkcode.selenium;
 
 public class TestHelper {
     public static String getBaseUrl() {
+        // return "http://selenium.thinkcode.se";
         // return "http://web-samples.herokuapp.com";
         return "http://localhost:8080";
     }
