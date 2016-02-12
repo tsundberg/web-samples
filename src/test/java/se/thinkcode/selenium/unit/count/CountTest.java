7436aaa2efbc3b0dc9ea9238de9a3bc8f228dec0
Thomas Sundberg <tsu@kth.se>
2016-02-12 12:28:46 +0200

    PageObject Refactoring

--- src/test/java/se/thinkcode/selenium/unit/count/CountTest.java
+++ src/test/java/se/thinkcode/selenium/unit/count/CountTest.java
@@ -1,32 +1,50 @@
 package se.thinkcode.selenium.unit.count;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-import se.thinkcode.selenium.ApplicationHelper;
-import se.thinkcode.selenium.TestHelper;
 
 public class CountTest {
     private WebDriver browser;
 
+    public static class BuyCurrency {
+        @FindBy(how = How.ID, using = "toCurrency")
+        private WebElement buyOption;
+        private WebDriver browser;
+
+        public BuyCurrency(WebDriver browser) {
+            this.browser = browser;
+            PageFactory.initElements(this.browser,this);
+        }
+
+        public void setBuyOption(String value) {
+        Select selectBuyCurrency = new Select(buyOption);
+        selectBuyCurrency.selectByValue(value);
+
+    }
+
+    }
+
+
     @Test
-    public void testBuyOneDollar ()
-    {
+    public void testBuyOneDollar() {
         //Go to Link
         browser = new FirefoxDriver();
         browser.get("http://localhost:4040/buyCurrency");
+
+        BuyCurrency buyCurrency = new BuyCurrency(browser);
         //Select Buy
         WebElement buyOption = browser.findElement(By.id("buy"));
         buyOption.click();
 
         //Set Buy USD
-        Select selectBuyCurrency = new Select(browser.findElement(By.id("toCurrency")));
-        selectBuyCurrency.selectByValue("USD");
+      buyCurrency.setBuyOption("USD");
 
         // Type 1 into the input field
         WebElement inputAmount = browser.findElement(By.id("amount"));
@@ -40,10 +58,9 @@ public class CountTest {
 
         //Click Submit Button
         WebElement submitButton = browser.findElement(By.id("submit"));
-         submitButton.click();
+        submitButton.click();
 
         //Validate the content
-
 
 
     }
